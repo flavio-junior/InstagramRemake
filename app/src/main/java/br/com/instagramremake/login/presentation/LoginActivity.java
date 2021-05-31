64880775b140ea6c5cebcaffe827b55328442ec3
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-05-31 19:58:41 -0300

    Validação de Formulários

--- app/src/main/java/br/com/instagramremake/login/presentation/LoginActivity.java
+++ app/src/main/java/br/com/instagramremake/login/presentation/LoginActivity.java
@@ -1,8 +1,13 @@
 package br.com.instagramremake.login.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import br.com.instagramremake.R;
 
@@ -12,7 +17,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
+        TextInputLayout inputLayoutTest = findViewById(R.id.login_edit_text_email_input);
+        inputLayoutTest.setError("Esse email é inválido");
+
+        EditText editText = findViewById(R.id.login_edit_text_email);
+        editText.setBackground(ContextCompat.getDrawable(LoginActivity.this,
+                R.drawable.edit_text_background_error));
+    }
 
 }
