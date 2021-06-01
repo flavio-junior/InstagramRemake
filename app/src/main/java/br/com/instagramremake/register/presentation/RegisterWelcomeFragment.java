9319919e28153e68057ca9205fee4a5b4f205edd
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-06-01 12:00:23 -0300

    Fragmento Bem-Vindo

---
+++ app/src/main/java/br/com/instagramremake/register/presentation/RegisterWelcomeFragment.java
@@ -0,0 +1,26 @@
+package br.com.instagramremake.register.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.instagramremake.R;
+
+public class RegisterWelcomeFragment extends Fragment {
+
+    public RegisterWelcomeFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_register_welcome, container, false);
+    }
+
+}
