4a25a264ccf53a77ab0723526843e2751afdc706
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-06-02 14:25:53 -0300

    Layout de Galeria de Fotos

--- app/src/main/java/br/com/instagramremake/main/camera/presentation/CameraFragment.java
+++ app/src/main/java/br/com/instagramremake/main/camera/presentation/CameraFragment.java
@@ -19,7 +19,7 @@ public class CameraFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_gallery, container, false);
         return view;
     }
 
