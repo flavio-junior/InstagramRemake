e63efad83e307897ee31a70cfd216b4a84f01bed
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-06-02 13:06:56 -0300

    Fragmento da Home

--- app/src/main/java/br/com/instagramremake/main/home/presentation/HomeFragment.java
+++ app/src/main/java/br/com/instagramremake/main/home/presentation/HomeFragment.java
@@ -1,4 +1,4 @@
-package br.com.instagramremake.main.profile.presentation;
+package br.com.instagramremake.main.home.presentation;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,13 +12,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import br.com.instagramremake.R;
+import br.com.instagramremake.main.profile.presentation.ProfileFragment;
 
-public class ProfileFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
-    public ProfileFragment() {
+    public HomeFragment() {
 
     }
 
@@ -31,18 +33,32 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_home, container, false);
 
-        RecyclerView recyclerView = view.findViewById(R.id.profile_recycler);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        RecyclerView recyclerView = view.findViewById(R.id.home_recycler);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(new PostAdapter());
         return view;
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         inflater.inflate(R.menu.menu_profile, menu);
         super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    private static class PostViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView imagePost;
+
+        public PostViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imagePost = itemView.findViewById(R.id.profile_image_grid);
+        }
+
+        public void bind(int image) {
+            this.imagePost.setImageResource(image);
+        }
     }
 
     private class PostAdapter extends RecyclerView.Adapter<PostViewHolder> {
@@ -82,8 +98,8 @@ public class ProfileFragment extends Fragment {
 
         @NonNull
         @Override
-        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-            return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_profile_grid, viewGroup, false));
+        public PostViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return new PostViewHolder(getLayoutInflater().inflate(R.layout.item_post_list, viewGroup, false));
         }
 
         @Override
@@ -95,20 +111,5 @@ public class ProfileFragment extends Fragment {
         public int getItemCount() {
             return images.length;
         }
-    }
-
-    private static class PostViewHolder extends RecyclerView.ViewHolder {
-
-        private final ImageView imagePost;
-
-        public PostViewHolder(@NonNull View itemView) {
-            super(itemView);
-            imagePost = itemView.findViewById(R.id.profile_image_grid);
-        }
-
-        public void bind(int image) {
-            this.imagePost.setImageResource(image);
-        }
-
     }
 }
