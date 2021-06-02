13bf0d847111b4d739742f19b4ebaee2228adb27
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-06-02 08:23:59 -0300

    Perfil de Usuário

---
+++ app/src/main/java/br/com/instagramremake/main/profile/presentation/ProfileFragment.java
@@ -0,0 +1,22 @@
+package br.com.instagramremake.main.profile.presentation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.instagramremake.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main_profile, container, false);
+    }
+
+}
